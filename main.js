c122906aef18076ef0d62ade974f645ce40617ae
flitterio <francesca@litterio.net>
2021-04-30 19:08:04 -0400

    added functionality to code. It currently works, has console.logs to show the construction of each number into phonetic equivalents. Needs to be cleaned up before pushing to main

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const numKey = {1: 'One', 2: 'Two', 3: 'Three', 4:'Four', 5:'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9:'Nine', 10: 'Ten'};
+const numKey = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4:'Four', 5:'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9:'Nine'};
 
 function convertToPhonetic(array){
-    return 'function is called'
+    let phonetic = []
+    let tempNumber = []
+    for(let i = 0; i < array.length; i++){
+        let numString = array[i].toString();
+        for(let j = 0; j < numString.length; j++){
+            tempNumber.push(numKey[numString.charAt(j)])
+            console.log('temp', tempNumber)
+        }
+        phonetic.push(tempNumber.join(""))
+        tempNumber = []
+    }
+    return phonetic
 };
 
-console.log('numKey with key 2 is', numKey[2]);
 console.log(convertToPhonetic([3, 25, 209]));
+console.log(convertToPhonetic( [10, 300, 5]));
