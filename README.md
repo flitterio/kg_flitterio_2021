8f3d25d8b499900bdf4d12b383828c3f5982d432
flitterio <francesca@litterio.net>
2021-04-30 18:32:40 -0400

    created const numKey as list of objects for easy access to phonetic equivalent, created structure of function convertToPhonetic, added console.logs to test both are returning as expected

---
+++ README.md
@@ -0,0 +1,7 @@
+### Hello Kargo
+
+# Here is the solution to my code
+
+Thank you for your consideration.
+
+Francesca 
---
+++ main.js
@@ -0,0 +1,8 @@
+const numKey = {1: 'One', 2: 'Two', 3: 'Three', 4:'Four', 5:'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9:'Nine', 10: 'Ten'};
+
+function convertToPhonetic(array){
+    return 'function is called'
+};
+
+console.log('numKey with key 2 is', numKey[2]);
+console.log(convertToPhonetic([3, 25, 209]));
