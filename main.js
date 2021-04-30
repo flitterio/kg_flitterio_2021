689bc9a77bfa94338bf687f1a9a7a685d82168ab
flitterio <francesca@litterio.net>
2021-04-30 19:14:39 -0400

    cleaned code, added comments, formatted final output to match expected output

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ const numKey = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4:'Four', 5:'Five', 6
 function convertToPhonetic(array){
     let phonetic = []
     let tempNumber = []
+
+    // iterate through array
     for(let i = 0; i < array.length; i++){
+
+        //convert each number to a string so that each digit can be accessed 
+
         let numString = array[i].toString();
+
+        //iterate through the digits in each number in the array
         for(let j = 0; j < numString.length; j++){
+
+            //use current digit as the key, push the corresponding value to the tempNumber array
             tempNumber.push(numKey[numString.charAt(j)])
-            console.log('temp', tempNumber)
         }
+
+        //join the separate, converted digits together and push those to the final phonetic array
         phonetic.push(tempNumber.join(""))
+
+        //reset the tempnumber 
         tempNumber = []
     }
-    return phonetic
+
+    //return the final array joined to match the expected format
+    return phonetic.join()
 };
 
 console.log(convertToPhonetic([3, 25, 209]));
